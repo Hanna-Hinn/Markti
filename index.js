2eee936f2ee09182c4cd0741fbf42296284e5a43
Sajed-AbdAlkareem <115458755+sajed88@users.noreply.github.com>
2023-03-22 17:48:55 +0200

    Demo

---
+++ index.js
@@ -0,0 +1,11 @@
+let serach = document.querySelector(".input-field");
+let serachValue ;
+// value of serach
+serach.addEventListener("keyup",(e) => {
+    if (e.key == "Enter" && serach.value != "") {
+        serachValue = serach.value;  
+        console.log(serachValue)      
+    }
+    });
+
+    
