9356185525ad4c47978ee11d1118b41511664cbe
Sajed-AbdAlkareem <sajedbaqa130@gmail.com>
2023-03-26 02:27:33 +0300

    design page-one
    add main.js and footer.js and main.css and footer.js in folder src
    and add link icon in index.html

---
+++ frontend/src/footer.js
@@ -0,0 +1,39 @@
+function footer(){
+return <nav className="footer">
+
+<nav className="text-head">
+<h1>SiteLogo</h1>
+<h1>Use Cases</h1>
+<h1>Company</h1>
+<h1>Follow Us</h1>
+</nav>
+
+<nav className="col1">
+<span>High level experience in web design and development</span>
+<span>Knowledge,producing quality work.</span>
+</nav>
+
+<nav className="col2">
+<a href="#">Web-designers</a>
+<a href="#">Marketers</a>
+<a href="#">Small Business</a>
+<a href="#">Website Builder</a>
+</nav>
+
+<nav className="col3">
+<a href="#">About Us</a>
+<a href="#">Careers</a>
+<a href="#">FAQs</a>
+<a href="#">Teams</a>
+<a href="#">Contact Us</a>
+</nav>
+
+<nav className="col4">
+<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+</nav>
+
+</nav>
+}
+export default footer;
---
+++ frontend/src/main.js
@@ -0,0 +1,90 @@
+function main(){
+    let flag1 = false
+    let flag2 = false    
+    let flag3 = false
+    function btnClick(){    
+           if(flag1==false){
+            let filter = document.querySelector(".search-icon")        
+            let boxbtn = `<nav class="box-filter"> <span>Ali Baba</span> 
+            <i class="fa fa-times" aria-hidden="true"></i>
+            </nav>`
+            filter.insertAdjacentHTML("afterend",boxbtn)
+            let x =  document.querySelector(".fa-times")
+            let y = document.querySelector(".box-filter") 
+            flag1 = true  
+            x.addEventListener("click",function(){
+                y.remove()
+                flag1 = false
+            })
+           
+           }
+                
+    }   
+    
+    function btnClick2(){  
+        if(flag2 == false){
+            let filter = document.querySelector(".search-icon")
+        let boxbtn = `<nav class="box-filter"> <span>amzone</span> 
+        <i class="fa fa-times" aria-hidden="true"></i>
+        </nav>`
+        filter.insertAdjacentHTML("afterend",boxbtn)
+        let x =  document.querySelector(".fa-times")
+        let y = document.querySelector(".box-filter")
+        flag2 = true
+        x.addEventListener("click",function(){
+            y.remove()
+            flag2 = false
+        })
+        
+        }
+                
+    }
+    
+    function btnClick3(){ 
+        if(flag3 == false){
+            let filter = document.querySelector(".search-icon")
+            let boxbtn = `<nav class="box-filter"> <span class="ali3">Ali express</span> 
+            <i class="fa fa-times" aria-hidden="true"></i>
+            </nav>`
+            filter.insertAdjacentHTML("afterend",boxbtn)
+            let x =  document.querySelector(".fa-times")
+            let y = document.querySelector(".box-filter")
+            flag3 = true
+            x.addEventListener("click",function(){
+                y.remove()
+                flag3 = false
+            })
+            
+        }
+                
+    }
+
+     function serach(e){
+        let serach = document.querySelector(".input-field");
+        let serachValue ;
+        if (e.key == "Enter" && serach.value != "") {
+            serachValue = serach.value;              
+     }
+
+  }
+
+
+return <nav class="home"><i class="fa fa-home" aria-hidden="true"></i>
+
+<nav class="search-icon">           
+<input type="search" class="input-field" onKeyUp={serach} placeholder="Search for files, plugins,and creators" ></input>
+<i class="fa fa-search" aria-hidden="true"></i>
+</nav>
+
+<nav class="filters">
+<h1>Stores:</h1>
+<button class="btn1" onClick={btnClick}>Ali baba</button>
+<button class="btn2" onClick={btnClick2}>amzone</button>
+<button class="btn3" onClick={btnClick3}>Ali express</button>
+</nav>
+
+</nav>
+
+}
+
+export default main;
