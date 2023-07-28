d3261a54a22222cc4ee253223c786aa8209d62ca
fadi tarazi <tarazifadi2001@gmail.com>
2023-07-28 14:25:42 +0300

    fixed
    Test it

--- backend/project/base/watchDog.py
+++ backend/project/base/watchDog.py
@@ -1,67 +1,75 @@
 import threading as th
-from . import storeApis
+from . import storeApis as variables
 from .Objects.VariablesDTO import VariablesDTO
 
+
 def start(var_dto: VariablesDTO):
-    # Loop through the storeList
+    """
+    This function creates a thread for each store name in the storeList
+    It then starts the thread and waits for it to finish
+    adding the results to the dataQueue
+    Parameters
+    ----------
+    storeList : list
+        list of store names
+
+    Returns
+    -------
+    None
+    """
+   # print("watchdog started")
+
+    # loop through the function list
+
+    # print("keyword:",var_dto.keyword,"\n")
+
     for storeName in var_dto.storeList:
-        # Check if the function is in variables.API_Dectionary
-        func = storeApis.API_Dectionary.get(storeName, None)
-        if func is not None:
-            # Define a separate thread function for each store name
+        # check if the function is in variables.API_Dectionary
+        if storeName in variables.API_Dectionary:
+            # define a separate thread function for each store name
             def myThread(store):
-                try:
-                    # Run the function and store the result
-                    print("keyword:", var_dto.keyword, "\n")
-                    result = func(var_dto.keyword)
+                # get the corresponding function based on store name
+                func = variables.API_Dectionary[store]
 
-                    # Send the store name and the result to the dataQueue
-                    # If the result is not a string (error), then send the result
-                   # print("result:", result, "\n")
-                    if (type(result) != str):
-                        if (len(result) == 0):
-                            var_dto.emptyApi += 1
-                            var_dto.emptyApiList.append(store)
-                        else:
-                            var_dto.dataQueue.put([store, result])
+                # run the function and store the result
+                print("keyword:", var_dto.keyword, "\n")
+                result = func(var_dto.keyword)
+
+                # send the store name and the result to the dataQueue
+                # if the result is not a string (error) then send the result
+                if (type(result) != str):
+                    if (len(result) == 0):
+                        var_dto.emptyApi += 1
+                        var_dto.emptyApiList.append(store)
+                      
                     else:
-                        var_dto.dataQueue.put([store, "error"])
+                        var_dto.dataQueue.put([store, result])
+                else:
+                    var_dto.dataQueue.put([store, "error"])
 
-                except Exception as e:
-                    var_dto.error = f"Error: {str(e)}" + " in " + store +"thread"
-                    return f"Error in threads: {str(e)}"
-
-            # Start a thread for each store name
+            # start a thread for each store name
             t = th.Thread(target=myThread, args=(storeName,))
             t.name = storeName
+
             t.start()
-            # Add the thread to the list
+            # add the thread to the list
             var_dto.threads.append(t)
         else:
-            # Return an error if the function is not in variables.API_Dectionary
+            # return an error if the function is not in variables.API_Dectionary
             var_dto.error = f"Error: {storeName} is not in variables.API_Dectionary"
             return f"Error: {storeName} is not in variables.API_Dectionary"
 
-    # Wait for all the threads to finish
+
+    # wait for all the threads to finish
     for t in var_dto.threads:
         t.join()
-
     print("requestedApiAmount(before empty check):", var_dto.requestedApiAmount)
     var_dto.requestedApiAmount = len(var_dto.storeList) - var_dto.emptyApi
-    if var_dto.error == "":
-            if var_dto.requestedApiAmount == 0:
-                 var_dto.error = "ALL APIS ARE EMPTY"
-    else:
-        print("error:", var_dto.error)
-        var_dto.requestedApiAmount = 0
-
-
-    
-        
-
-
-    
-    
-    # Set the requestedApiAmount to the length of the function list
+    if var_dto.requestedApiAmount == 0:
+        var_dto.error = "ALL APIS ARE EMPTY"
+ 
+    # set the requestedApiAmount to the length of the function list
+   
     print("requestedApiAmount(after checking if empty):", var_dto.requestedApiAmount)
+
     print("\n", "watchdog finished")
