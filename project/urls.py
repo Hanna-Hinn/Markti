bb06ad194e1e490e5da7abf24508eff5adb956f8
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-07-30 17:17:41 +0300

    removed favicon url

--- project/urls.py
+++ project/urls.py
@@ -18,17 +18,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
-
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='index.html')),
     path('api/', include('base.urls')),
-    path(r'^favicon\.ico$', favicon_view)
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
